75c439c99da8141b70f8273e7549258e8d3782ef
Vinicius Teixeira <99350364+viniciushammett@users.noreply.github.com>
2023-01-25 15:09:56 -0300

    Upload Files

---
+++ api-go-rest/controllers/controllers.go
@@ -0,0 +1,31 @@
+package controllers
+
+import (
+	"api-go-rest/models"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Home Page")
+
+}
+
+func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(models.Personalidades)
+}
+
+func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for _, personalidade := range models.Personalidades {
+		if strconv.Itoa(personalidade.Id) == id {
+			json.NewEncoder(w).Encode(personalidade)
+		}
+	}
+}
---
+++ api-go-rest/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"api-go-rest/models"
+	"api-go-rest/routes"
+	"fmt"
+)
+
+func main() {
+	models.Personalidades = []models.Personalidade{
+		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
+		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
+	fmt.Println("Iniciando o servidor Rest com Go")
+	routes.HandleRequest()
+}
